7cca5e1cc362eaf9ba418d72a2273f6b8d7f300c
ac2917 <aditya.chaturvedi17@imperial.ac.uk>
2018-06-25 11:43:43 +0100

    clean up main and query

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import query
 import pandas
 
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
+first_block = 400000
+last_block = 400000
 
-first_block = 364133
-last_block = 364133
+result = query.query_database(query.get_block_data(first_block,first_block))
 
-result = query.query_database(first_block, last_block)
-df = pandas.DataFrame(result)
+if(result is None):
+    print("Something went wrong, there is no data for this/these blocks")
+
+df = result.to_data_frame()
 df.to_csv('block_data.csv', encoding='utf-8', index=False)
 
-print('I did something too')
-# Convert DataFrame to matrix
-mat = df.values
+print(df.columns[0])
+
--- query.py
+++ query.py
@@ -1,13 +1,5 @@
 import py2neo
-import pandas
-from sklearn.cluster import KMeans
-
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
-
 import urllib3
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def query_database(query):
@@ -15,25 +7,23 @@ def query_database(query):
     graph_db = py2neo.Graph("https://dsi-bitcoin.doc.ic.ac.uk:7473/db/data/", auth=("guest_ro", "imperialO_nly"))
     return graph_db.run(query)
 
-def simple_query():
+def check_block(first_block, last_block):
     query_string = """ 
-                    MATCH (t:Tx) -[:MINED_IN]-> (b:Block) WHERE b.height=400000 RETURN b
-                    """
+                    MATCH (b:Block) 
+                    WHERE b.height >= {} AND b.height <= {}
+                    RETURN b
+                    """.format(first_block, last_block)
     return query_string
 
-def project_graph(algorithm, node, value):
-
+def get_block_data(first_block, last_block):
     query_string = """
-                    CALL algo.{}(
-                    'MATCH ({}) RETURN id({}) AS id',
-                    "MATCH (n)-->(m) RETURN id(n) AS source, id(m) AS target",
-                    {graph: "cypher"})
-                    """.format(algorithm, node, value)
+                    MATCH (b:Block) <-[:MINED_IN]- (t:Tx) <-[:IN]- (txi:TxIn) <-[:UNLOCK]- (iadr:Address)
+                    WHERE b.height >= {} AND b.height <= {}
+                    MATCH (txi) <-[:SPENT]- (txo1:TxOut) 
+                    MATCH (oadr:Address) <-[:LOCK]- (txo2:TxOut) <-[:OUT]- (t)
+                    RETURN iadr.address, txo1.value, ID(txo1), ID(txi), ID(t), ID(txo2), txo2.value, oadr.address
+                    """.format(first_block, last_block)
     return query_string
 
-
-result = query_database(simple_query())
-
-result.data()
 
 
